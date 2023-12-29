9735afd7c9454949464a30ec180de73045e05521
Gokul.V <142217024+GokulV2795@users.noreply.github.com>
2023-12-29 11:18:38 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello, GWC it's my new CODE"
+    return "Hello, GWC it's my new CODE it's my New code version"
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
